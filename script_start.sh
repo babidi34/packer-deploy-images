50cf8191b3d389b056e7c18ba90ba1a98f4cff91
babidi34 <karimbaidi@gmail.com>
2022-03-25 00:27:23 +0100

    box vagrant OK

--- script_start.sh
+++ script_start.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 # Deploy keys to allow all nodes to connect each others as root
 mkdir /root/.ssh/
+mkdir -p /home/vagrant/.ssh/
 mv /tmp/id_rsa*  /root/.ssh/
+cp /root/.ssh/id_rsa* /home/vagrant/.ssh/
 
 chmod 400 /root/.ssh/id_rsa*
+chmod 400 /home/vagrant/.ssh/id_rsa*
 chown root:root  /root/.ssh/id_rsa*
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
 
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 chmod 400 /root/.ssh/authorized_keys
+chmod 400 /home/vagrant/.ssh/authorized_keys
 chown root:root /root/.ssh/authorized_keys
+chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 
 # Install updates and curl
 apt update -y
@@ -21,3 +28,6 @@ apt update -y
 truncate -s 0 /etc/machine-id
 rm /var/lib/dbus/machine-id
 ln -s /etc/machine-id /var/lib/dbus/machine-id
+
+## add vagrant to sudo
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
