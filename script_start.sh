a3181333730a0411f2fd96176d173b1b4c05061f
babidi34 <karimbaidi@gmail.com>
2024-01-16 20:42:49 +0100

    fix script (error ssh key name)

--- script_start.sh
+++ script_start.sh
@@ -2,16 +2,16 @@
 # Deploy keys to allow all nodes to connect each others as root
 mkdir /root/.ssh/
 mkdir -p /home/vagrant/.ssh/
-mv /tmp/id_rsa*  /root/.ssh/
-cp /root/.ssh/id_rsa* /home/vagrant/.ssh/
+mv /tmp/id_ecdsa*  /root/.ssh/
+cp /root/.ssh/id_ecdsa* /home/vagrant/.ssh/
 
-chmod 400 /root/.ssh/id_rsa*
-chmod 400 /home/vagrant/.ssh/id_rsa*
-chown root:root  /root/.ssh/id_rsa*
-chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
+chmod 400 /root/.ssh/id_ecdsa*
+chmod 400 /home/vagrant/.ssh/id_ecdsa*
+chown root:root  /root/.ssh/id_ecdsa*
+chown vagrant:vagrant /home/vagrant/.ssh/id_ecdsa*
 
-cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+cat /root/.ssh/id_ecdsa.pub >> /root/.ssh/authorized_keys
+cat /home/vagrant/.ssh/id_ecdsa.pub >> /home/vagrant/.ssh/authorized_keys
 chmod 400 /root/.ssh/authorized_keys
 chmod 400 /home/vagrant/.ssh/authorized_keys
 chown root:root /root/.ssh/authorized_keys
